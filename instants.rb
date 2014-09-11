e11d785a3bcf897bab45f0a439364fa3037a4d91
Yannick <yannick.schutz@gmail.com>
2014-09-11 08:52:45 +0200

    Force cache

--- instants.rb
+++ instants.rb
@@ -2,29 +2,27 @@ require "json"
 
 class Instants < Sinatra::Base
 
-  if memcache_servers = ENV["MEMCACHE_SERVERS"]
-    cache = Dalli::Client.new
+  cache = Dalli::Client.new
 
-    use Rack::Cache,
-      verbose: true,
-      metastore:   cache,
-      entitystore: cache
-  end
+  use Rack::Cache,
+    verbose:     true,
+    metastore:   cache,
+    entitystore: cache
 
   get '/' do
-    cache_control :public, max_age: 1800
+    cache_control :public, max_age: 3600
     erb :index, locals: { instants: dropbox_client.metadata("/")["contents"] }
   end
 
   get "/:path" do
-    cache_control :public, max_age: 1800
+    cache_control :public, max_age: 3600
     folder = dropbox_client.metadata("/#{params[:path]}", 25000, true, nil, nil, false, true)
     pictures = folder["contents"].select { |e| e["thumb_exists"] == true && !e["path"].match(/_cover\./)}
     erb :instant, locals: { pictures: pictures }
   end
 
   get "/thumbs/*" do
-    cache_control :public, max_age: 1800
+    cache_control :public, max_age: 36000
     t, metadata = dropbox_client.thumbnail_and_metadata("/#{params[:splat].first}", "xl")
     content_type metadata["mime_type"]
     t
