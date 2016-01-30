bd98a2c2ba36ed045d9218b129b1f61a7c6e2fda
Yannick <yannick.schutz@gmail.com>
2016-01-30 19:42:46 +0100

    Do not redirect pages named b* or m*

--- lib/endpoints/redirections.rb
+++ lib/endpoints/redirections.rb
@@ -1,12 +1,12 @@
 module Endpoints
   class Redirections < Base
     subdomain false do
-      get "/b*" do
-        redirect "#{protocol}://blog.#{ENV['DOMAIN']}#{params["splat"][0]}"
+      get "/b/*" do
+        redirect "#{protocol}://blog.#{ENV['DOMAIN']}/#{params["splat"][0]}"
       end
 
-      get "/m*" do
-        redirect "#{protocol}://moments.#{ENV['DOMAIN']}#{params["splat"][0]}"
+      get "/m/*" do
+        redirect "#{protocol}://moments.#{ENV['DOMAIN']}/#{params["splat"][0]}"
       end
 
       def protocol
